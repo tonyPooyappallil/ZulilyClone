51791b06ceb5eccef03991c304fae2b7e58d8c40
Antony Chacko <antony101tony@gmail.com>
2021-08-25 22:24:35 +0530

    homepage visually complete

--- homepage/home.js
+++ homepage/home.js
@@ -6,3 +6,30 @@ setTimeout(function () {
   videoDiv.innerHTML = "";
   videoDiv.append(img);
 }, 3500);
+
+const buttonRight = document.getElementById("slideRight");
+const buttonLeft = document.getElementById("slideLeft");
+
+buttonRight.onclick = function () {
+  document.getElementById("slideContainer").scrollLeft += 900;
+  console.log(document.getElementById("slideContainer").scrollLeft);
+  console.log(document.getElementById("slideContainer").length);
+};
+buttonLeft.onclick = function () {
+  document.getElementById("slideContainer").scrollLeft -= 900;
+};
+
+const bdy = document.getElementById("bdy");
+const sticky = document.getElementById("sticky-header");
+console.log(444);
+bdy.onscroll = function () {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 47) {
+    console.log("great than 500");
+
+    sticky.classList.add("fixed");
+  } else {
+    console.log("not great than 100");
+    sticky.classList.remove("fixed");
+  }
+};
